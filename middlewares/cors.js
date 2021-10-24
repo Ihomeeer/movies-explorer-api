623695b9f6e6326cd724cd6a7ab15110d8576fc4
Mikhail Kirichkov <ihomeer@gmail.com>
2021-10-24 20:04:06 +0300

    fix: обновил cors и ридми

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,5 +1,7 @@
 // Массив доменов, с которых разрешены кросс-доменные запросы
 const allowedCors = [
+  'http://ihomeer-movies.nomoredomains.monster',
+  'https://ihomeer-movies.nomoredomains.monster',
   'http://localhost:3000',
   'https://localhost:3000',
   'http://localhost:3001',
